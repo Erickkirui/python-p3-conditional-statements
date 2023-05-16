0f5071f418cf2f9c518f9023c12e935a5c46bcd5
Erick <erickkirui653@gmail.com>
2023-05-16 15:04:18 -0400

    fixed indentation

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-     if username == "admin" and password == "12345":
+    if username.lower() == "admin" and password == "12345":
         return "Access granted"
     else:
         return "Access denied"
-admin_login("ADMIN","12345")
 
 def hows_the_weather(temperature):
     if temperature < 40:
@@ -17,9 +16,8 @@ def hows_the_weather(temperature):
     else:
         return "It's perfect out there!"
 
-
 def fizzbuzz(num):
-     if num % 3 == 0 and num % 5 == 0:
+    if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
     elif num % 3 == 0:
         return "Fizz"
@@ -29,15 +27,15 @@ def fizzbuzz(num):
         return num
 
 def calculator(operation, num1, num2):
- match operation:
- case "+":
-  return num1 + num2
- case "-":
-  return num1 - num2
- case "*":
-  return num1 * num2
- case "/" :
-  return num1 + num2
- case _:
-  print("Invalid operation!")
-  return None
+    match operation:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+        case _:
+            print("Invalid operation!")
+            return None
